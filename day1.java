e3b9e4a541af78224074fd174818fd06a8e49863
Joseph Hsu <54673847+JHsu01@users.noreply.github.com>
2021-05-28 10:12:22 -0400

    day_1
    HR day_1

---
+++ day1.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String inputString = scanner.nextLine();
+        scanner.close();
+        
+        System.out.println("Hello, World.");
+        System.out.println(inputString);
+        
+        
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+    }
+}
